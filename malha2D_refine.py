334a0dd60b32835f0b32453101aeed4a66c1b21d
projetoemBR <57676561+projetoemBR@users.noreply.github.com>
2019-11-21 14:13:55 -0300

    Update malha2D_refine.py

--- malha2D_refine.py
+++ malha2D_refine.py
@@ -2,17 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 20 15:38:42 2019
-
-@author: akel
-"""
-"""
-Created on Wed Nov 20 12:27:13 2019
-
 @author: akel
 """
 
 from discretize import TreeMesh
 from discretize.utils import matutils, meshutils
+import SimPEG as simpeg
 import matplotlib.pyplot as plt
 import numpy as np
 plt.close('all') 
@@ -33,62 +28,42 @@ hx = [(dx, nbcx)]
 hy = [(dy, nbcy)]
 M = TreeMesh([hx, hy], x0=['C',-15000])
 
-
-
-
-## definir camada
+# definir camada
 xx = M.vectorNx
 yy = np.zeros(nbcx+1) #vetor linha(poderia ser uma função ou pontos)
 pts = np.c_[matutils.mkvc(xx), matutils.mkvc(yy)]
 M = meshutils.refine_tree_xyz(M, pts,octree_levels=[1, 1], method='box',finalize=False)
 
 
+#definir um ponto(xc,yc) a ser refinado.
+Raio_length=500
+xc=-5000
+yc=2200
 
-# Define corner points for rectangular box
-#xp, yp = np.meshgrid([-5000, 5000], [-150., -150])
-#xy = np.c_[matutils.mkvc(xp), matutils.mkvc(yp)]  # matutils.mkvc creates vectors
-#M = meshutils.refine_tree_xyz(
-#    M, xy, octree_levels=[1, 1], method='surface', finalize=False
-#    )
-#
-#xp, yp = np.meshgrid([-5000, 5000], [-350., -350])
-#xy = np.c_[matutils.mkvc(xp), matutils.mkvc(yp)]  # matutils.mkvc creates vectors
-#M = meshutils.refine_tree_xyz(
-#    M, xy, octree_levels=[1, 1], method='surface', finalize=False
-#    )
-
-Raio_length=5000
 xx = M.vectorNx
 yy = np.zeros(nbcx+1) #vetor linha(poderia ser uma função ou pontos)
 pts = np.c_[matutils.mkvc(xx), matutils.mkvc(yy)]
 def refine(cell):
-        if np.sqrt(((np.r_[cell.center]+0)**2).sum()) < Raio_length:
-            return 6
-        return 3
+        if np.sqrt(((np.r_[cell.center]+[xc,yc])**2).sum()) < Raio_length:
+            return 10
+        return 6
     
 M.refine(refine)
+
 M.finalize()  # Must finalize tree mesh before use
 
 #M.plotGrid(showIt=True)
 
 print("\n the mesh has {} cells".format(M))
-
 ccMesh=M.gridCC
 print('indices:',np.size(ccMesh))
 
-print("\n the mesh has {} cells".format(M))
-ccMesh=M.gridCC
-print('indices:',np.size(ccMesh))
-     
+
 conds = [1e-2,1]
-sig = simpeg.Utils.ModelBuilder.defineBlock(M.gridCC, [-5000 , -150], [5000, -350], conds)
+sig = simpeg.Utils.ModelBuilder.defineBlock(M.gridCC, [-5000 , -2200], [5000, -4200], conds)
 #       
 sig[M.gridCC[:, 1] > 0] = 1e-8
 sigBG = np.zeros(M.nC) + conds[1]
 sigBG[M.gridCC[:, 1] > 0] = 1e-8
 
 M.plotImage(np.log(sig), grid=True )
-
-
-
-
